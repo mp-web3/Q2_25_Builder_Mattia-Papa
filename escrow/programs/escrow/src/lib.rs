9650e04f6855d6df86112626f39e4d8618812a88
Mattia <mp.web3@gmail.com>
2025-04-12 14:04:45 +0200

    escrow files from lecture Week-2 Day-2

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,16 +1,56 @@
+#![allow(clippy::result_large_err)]
+
 use anchor_lang::prelude::*;
 
+pub mod instructions;
+pub mod state;
+
+pub use instructions::*;
+pub use state::*;
 declare_id!("7RwCakEJihTYJDvpQHkBnXZBuTgX9x51Wrg5Ut4DMRxm");
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+    
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeEscrow<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+  init,
+  space = 8 + Escrow::INIT_SPACE,
+  payer = payer
+  )]
+  pub escrow: Account<'info, Escrow>,
+  pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct CloseEscrow<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+  mut,
+  close = payer, // close account and return lamports to payer
+  )]
+  pub escrow: Account<'info, Escrow>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+  #[account(mut)]
+  pub escrow: Account<'info, Escrow>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Escrow {
+  count: u8,
+}
