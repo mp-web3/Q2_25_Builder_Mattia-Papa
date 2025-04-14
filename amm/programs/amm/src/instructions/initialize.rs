eff0a2204118388d5c72837595a99ae59c0e1cca
Mattia <mp.web3@gmail.com>
2025-04-14 12:43:18 +0200

    feat: Initialize instructions of amm

--- amm/programs/amm/src/instructions/initialize.rs
+++ amm/programs/amm/src/instructions/initialize.rs
@@ -1,9 +1,75 @@
 use anchor_lang::prelude::*;
 
-#[derive(Accounts)]
-pub struct Initialize {}
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
-    Ok(())
+use crate::state::Config;
+
+#[derive(Accounts)]
+#[instruction(seed: u64)]
+pub struct Initialize <'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub mint_x: Account<'info, Mint>,
+    pub mint_y: Account<'info, Mint>,
+    #[account(
+        init, 
+        payer = admin,
+        seeds = [b"lp", config.key.as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = config
+    )]
+
+    pub mint_lp: Account<'info, Mint>,
+
+    // In this case we don't want to support interface, but only token '2022'
+    #[account(
+        init,
+        payer = admin,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = admin,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        init, 
+        payer = admin,
+        seeds = [b"config", seed.to_le_bytes().as_ref()],
+        bump,
+        space = Config::INIT_SPACE,
+    )]
+    pub config: Account<'info, Config>,
+
+    pub token_program: Program<'info, Config>,
+    pub associated_token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, seed: u64, fee: u16, authority: Option<Pubkey>, bumps: &InitializeBumps) -> Result<()> {
+        self.config.set_inner(Config {
+            seed,
+            authority,
+            mint_x: self.mint_x.key(),
+            mint_y: self.mint_y.key(),
+            fee,
+            locked: false,
+            config_bump: bumps.config,
+            lp_bump: bumps.mint_lp,
+        });
+        
+        Ok(())
+    }
 }
